11780e25381ac53392ff705b96f9da7845acf21d
Joseph Siracusa <>
2021-04-13 13:09:48 +0000

    1. Created and linked files for responsive CSS
    2. Added favicon
    
    3. Cleaned up project by deleting unnecessary files

--- scripts/download.js
+++
@@ -1,7 +0,0 @@
-// var anchor = document.getElementById('link-test');
-// anchor.href = "downloads/test.txt";
-// anchor.target = '_blank';
-// anchor.download = "test";
-// anchor.click();
-
-// console.log('done');
